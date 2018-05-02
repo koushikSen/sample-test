94ba0dba3417ad53cf67218752fcef8fe530dedb
koushik sen <imkoushiksen@hotmail.com>
2018-05-02 18:05:02 +0530

    reupload vendor

---
+++ src/CalculatorController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Koushik\Sample;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class CalculatorController extends Controller
+{
+    //
+    public function add($a, $b){
+    	$result =  $a + $b;
+    	return view('calculator::add', compact('result'));
+    }
+
+    public function subtract($a, $b){
+    	echo $a - $b;
+    }
+}
---
+++ src/routes.php
@@ -0,0 +1,4 @@
+<?php
+
+Route::get('add/{a}/{b}', 'Koushik\Sample\CalculatorController@add');
+Route::get('subtract/{a}/{b}', 'Koushik\Sample\CalculatorController@subtract');
